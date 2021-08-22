a87b9417231776820a6e5812a28d3dc15471f030
kazumasa <70145199+Kazumasa1@users.noreply.github.com>
2021-08-22 14:23:06 +0900

    textpair_apiの導入

--- js/textpair.js
+++ js/textpair.js
@@ -16,7 +16,15 @@ const port = 3000;
 const server = http.createServer(RouteSetting);
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+    console.log(`Server running at http://${hostname}:${port}/`);
+    
+    axios(options)
+    .then((res) => {
+        console.log(res.data);
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 });
 
 function RouteSetting(req, res) {
@@ -53,3 +61,25 @@ function RouteSetting(req, res) {
       break;
   }
 }
+
+
+const axios = require(`axios`);
+const APIKEY = `***REMOVED***`; //API KEY
+const URL = `https://labs.goo.ne.jp/api/textpair`;
+// const SENTECE = process.argv[2];
+// const OUTPU_TYPE = `katakana`; //or `hiragana`
+
+const options = {
+    method: 'post',
+    url: URL,
+    headers: {
+        'Content-Type': `application/x-www-form-urlencoded`,
+        'Content-Type': `application/json`
+    },
+    data: {
+        app_id: APIKEY,
+        request_id: 'u22',
+        text1: "僕は大人になったらペンギンを飼いたい",
+        text2: "僕は大人になったらアザラシを飼いたい"
+    }
+};
