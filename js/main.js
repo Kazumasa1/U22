932afeacf78a1b6e250eabdd09c7cedadbaf745f
kazumasa <70145199+Kazumasa1@users.noreply.github.com>
2021-08-23 13:43:26 +0900

    読み込んだファイルの配列をsliceで分割する

--- js/main.js
+++ js/main.js
@@ -59,30 +59,58 @@ function toTextFile(){
 
 const text = document.getElementById("text-file");
 let inputTextFileArray = [];
+let compTextArray = [];
+let TextArray_len = 0;
+
+const COMP_TEXT_LEN = 5;
+
+let slice_st = 0;
+let slice_ed = 0;
 
 //ダイアログでファイルが選択された時
 text.addEventListener("change", function (event) {
-
+    
     const file = event.target.files;
-
+    
     //FileReaderの作成
     const reader = new FileReader();
     //テキスト形式で読み込む
     reader.readAsText(file[0]);
-
+    
     //読込終了後の処理
     reader.onload = function () {
         //テキストエリアに表示する
         // text.value = reader.result;
         inputTextFileArray += reader.result;
         inputTextFileArray = inputTextFileArray.split(',');
-        console.log(inputTextFileArray);
-        console.log(inputTextFileArray2);
-        console.log(inputTextFileArray.length);
+        TextArray_len = inputTextFileArray.length;
+        
+        
+        if(TextArray_len <=  COMP_TEXT_LEN) {
+            // 選択されたテキストファイルの文字列の数が比較文字数以下のときの処理
+            slice_ed = TextArray_len;
+            compTextArray += inputTextFileArray.slice(slice_st, slice_ed);
+            console.log(compTextArray);
+            console.log('true');
+        }else {
+            // 選択されたテキストファイルの文字列の数が比較文字数より多いときの処理
+            slice_ed = COMP_TEXT_LEN;
+            
+            let compTextArray_len = Math.floor(TextArray_len / COMP_TEXT_LEN);
+            for (let i = 0; i <=  compTextArray_len; i++) {
+                compTextArray.push(inputTextFileArray.slice(slice_st, slice_ed));
+                // compTextArray += inputTextFileArray.slice(slice_st, slice_ed);
+                slice_st = slice_ed;
+                slice_ed += COMP_TEXT_LEN;
+            }
+            console.log('false');
+        }
+        console.log(compTextArray);
     }
 });
 
 // setTimeout(textAdd, 40000);
+
 
 function textAdd(){
     // ここに読み込みが完了したら実行したい処理を記述する
