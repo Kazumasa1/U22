7b833d89a0e3bc3d3038e7d92367b70372fd2df8
kazumasa <70145199+Kazumasa1@users.noreply.github.com>
2021-08-26 21:54:29 +0900

    node.jsによる重複ジャッジ機能の実装

--- js/main.js
+++ js/main.js
@@ -15,43 +15,104 @@ recognition.continuous = true;
 let finalTranscript = '';
 // let toTextFileArray = '';
 let toTextFileArray = [];
+let new_meeting_text_array = [];
 
 const COMP_TEXT_LEN = 5;
+
+let compTextArray = [];
+
+let talk_count = 0;
+
+let old_text_num = 0;
+let new_text_num = 0;
+
+let post_count = 0;
+
+
+// let json_to_textpair = [
+//         compTextArray[old_text_num] + ',',
+//         new_meeting_text_array[new_text_num] + ','
+//     ];
 
 recognition.onresult = (event) => {
     let interimTranscript = '';
     for (let i = event.resultIndex; i < event.results.length; i++) {
     let transcript = event.results[i][0].transcript;
+    // console.log("load");
     if (event.results[i].isFinal) {
-        // toTextFileArray += transcript;
-        // toTextFileArray += ',';
-        toTextFileArray.push(transcript);
-        console.log(toTextFileArray);
+        talk_count++;
+        toTextFileArray += transcript;
+        toTextFileArray += ',';
+        // console.log(toTextFileArray);
         // console.log(toTextFileArray[0]);
         finalTranscript += transcript += '<br>';
+        // console.log(talk_count);
+        
+        if (talk_count >= COMP_TEXT_LEN) {
+            talk_count = 0;
+            old_text_num = 0;
+            post_count = 0;
+            new_meeting_text_array.push(toTextFileArray);
+            // console.log("compTextArray:"+compTextArray.length);
+            // console.log(new_meeting_text_array[0]);
+            // for (let j = 0; j < Number(compTextArray.length); j++){
+            // // for (let j = compTextArray.length; j < 0; j--){
+                
+            //     setInterval(postForm,250);
+            //     // postForm();
+            //     // console.log(old_text_num);
+            //     old_text_num++;
 
-        if (toTextFileArray.length >= COMP_TEXT_LEN) {
-            
+                const postFormInter = setInterval(() =>{
+                    postForm();
+                    if (post_count >= compTextArray.length) {
+                        clearInterval(postFormInter);
+                    }
+                },250);
+            // }
+            // new_text_num++;
+            // console.log("if-test");
+            // console.log(compTextArray.length);
         }
-    } else {
-        interimTranscript = transcript;
-    }
-    }
+        // console.log("test");
+        
+        } else {
+            interimTranscript = transcript;
+        }
+    }  
     resultDiv.innerHTML = finalTranscript + '<i style="color:#ddd;">' + interimTranscript + '</i>';
 
 }
-const postForm = value => {
 
+// function admain() {
+//     for (var i = 0; i < 2; i++) {
+//       var httpObj = new XMLHttpRequest(); //この行を変更
+//       httpObj.onreadystatechange = function () {
+//         if (httpObj.readyState == 4) {
+//           if (httpObj.status == 200) {
+//            //複数回実行される
+//           }
+//         }
+//       }
+  
+//       httpObj.open("get", "xmlhandler.ashx?id="+id[i], true);
+//       httpObj.send(json_to_textpair);
+//       break;
+//     }
+//   }
+
+async function postForm() {
+    
     let json_to_textpair = [
-        compTextArray[0] + ',',
-        compTextArray[1] + ','
+        compTextArray[old_text_num] + ',',
+        new_meeting_text_array[new_text_num] + ','
     ];
     
      
     xhr.open('POST', 'response');
     xhr.setRequestHeader('content-type', 'application/json;charset=UTF-8');
     
-
+    
     xhr.send( json_to_textpair );
     
     xhr.onreadystatechange = function() {
@@ -60,11 +121,41 @@ const postForm = value => {
      
             // console.log( xhr.responseText );
             console.log( xhr.response );
-          
+
+            
         }
     }
+    old_text_num++;
+    post_count++;
+}
 
-};
+// const postForm = value => {
+//     let json_to_textpair = [
+//         compTextArray[old_text_num] + ',',
+//         new_meeting_text_array[new_text_num] + ','
+//     ];
+    
+     
+//     xhr.open('POST', 'response');
+//     xhr.setRequestHeader('content-type', 'application/json;charset=UTF-8');
+    
+
+//     xhr.send( json_to_textpair );
+    
+//     xhr.onreadystatechange = function() {
+     
+//         if(xhr.readyState === 4 && xhr.status === 200) {
+     
+//             // console.log( xhr.responseText );
+//             console.log( xhr.response );
+          
+//         }
+//     }
+
+
+// };
+
+// postForm(); 
 
 function start(){
 
@@ -92,7 +183,6 @@ function toTextFile(){
 
 const text = document.getElementById("text-file");
 let inputTextFileArray = [];
-let compTextArray = [];
 let TextArray_len = 0;
 
 
@@ -122,9 +212,9 @@ text.addEventListener("change", function (event) {
             // 選択されたテキストファイルの文字列の数が比較文字数以下のときの処理
             slice_ed = TextArray_len;
             compTextArray += inputTextFileArray.slice(slice_st, slice_ed);
-            console.log(compTextArray);
-            console.log('true');
-            postForm();
+            // console.log(compTextArray);
+            // console.log('true');
+            // postForm();
         }else {
             // 選択されたテキストファイルの文字列の数が比較文字数より多いときの処理
             slice_ed = COMP_TEXT_LEN;
@@ -132,7 +222,7 @@ text.addEventListener("change", function (event) {
             
             let compTextArray_len = Math.floor(TextArray_len / COMP_TEXT_LEN);
             for (let i = 0; i <=  compTextArray_len; i++) {
-
+                
                 itijiArray = [];
                 // compTextArray.push(inputTextFileArray.slice(slice_st, slice_ed));
                 itijiArray += inputTextFileArray.slice(slice_st, slice_ed);
@@ -140,8 +230,9 @@ text.addEventListener("change", function (event) {
                 slice_st = slice_ed;
                 slice_ed += COMP_TEXT_LEN;
             }
-            console.log('false');
-            postForm();
+            // console.log(compTextArray);
+            // console.log('false');
+            // postForm();
         }
     }
 });
